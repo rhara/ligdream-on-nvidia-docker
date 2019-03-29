dbec2059bd0d6fb51ea99e9a75052c42b6679331
Ryuichiro Hara <hara.ryuichiro@gmail.com>
2019-03-29 13:29:28 +0900

    -

--- getData.sh
+++ getData.sh
@@ -18,3 +18,12 @@ chk() {
 
 chk zinc15_druglike_clean_canonical_max60.zip a36c036f36b0a31c63b115703c1430e5
 chk ligdream-20190128T143457Z-001.zip c1a4ca4f2e46ad86c0f9c6c606cc8a53
+
+mkdir -p traindataset
+unzip zinc15_druglike_clean_canonical_max60.zip -d traindataset
+
+rm -rf modelweights
+unzip ligdream-20190128T143457Z-001.zip
+mv ligdream modelweights
+
+echo "Complete"
